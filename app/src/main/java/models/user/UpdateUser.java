c3643d6a5380c0f921f6eca1f85ef4a17035d503
Dokriseum <eiki300499@gmail.com>
2021-01-08 21:24:56 +0100

    updated UserUpdate | more options

--- app/src/main/java/models/user/UpdateUser.java
+++ app/src/main/java/models/user/UpdateUser.java
@@ -39,6 +39,42 @@ public class UpdateUser {
 
     }
 
+    public boolean updateBirthdate(int id, String birthdate) {
+        ((User) this.storage.getList().get(id)).setFirstname(birthdate);
+        return true;
+
+    }
+
+    public boolean updateStreet(int id, String street) {
+        ((User) this.storage.getList().get(id)).setFirstname(street);
+        return true;
+
+    }
+
+    public boolean updateHouseNumber(int id, String house_number) {
+        ((User) this.storage.getList().get(id)).setFirstname(house_number);
+        return true;
+
+    }
+
+    public boolean updatePostCode(int id, String post_code) {
+        ((User) this.storage.getList().get(id)).setFirstname(post_code);
+        return true;
+
+    }
+
+    public boolean updateCity(int id, String city) {
+        ((User) this.storage.getList().get(id)).setFirstname(city);
+        return true;
+
+    }
+
+    public boolean updateCountry(int id, String country) {
+        ((User) this.storage.getList().get(id)).setFirstname(country);
+        return true;
+
+    }
+
     private boolean checkUsername(String username) {
         boolean isAvailable = true;
         for (Storable k : this.storage.getList()) {
